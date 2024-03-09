b34c40be068d1f58152ca4b3596e3da4aa349de1
Fityan <fityannugroho@gmail.com>
2024-03-09 15:42:10 +0700

    feat: improve performance by using p-queue to run the sync tasks concurrently

--- src/actions/sync.ts
+++ src/actions/sync.ts
@@ -7,8 +7,10 @@ import {
   villages,
 } from '@/db/schema';
 import { initProgressBar } from '@/utils/cli';
+import env from '@/utils/env';
 import { validateArea, type Areas, validateSchema } from '@/validation';
 import { and, eq, ilike, or } from 'drizzle-orm';
+import PQueue from 'p-queue';
 import { z } from 'zod';
 
 const optionsSchema = z
@@ -23,6 +25,8 @@ const optionsSchema = z
 type Options = z.infer<typeof optionsSchema> & {
   signal?: AbortSignal;
 };
+
+type Task = Parameters<InstanceType<typeof PQueue>['add']>[0];
 
 export const syncBoundaries = async (area: Areas, options?: Options) => {
   validateArea(area);
@@ -50,152 +54,176 @@ export const syncBoundaries = async (area: Areas, options?: Options) => {
     `${options?.force ? 'Force syncing' : 'Syncing'} ${unsyncAreas.length} ${area} boundaries`,
   );
 
+  const queue = new PQueue({ concurrency: env.CONCURRENCY });
   const progressBar = initProgressBar({ total: unsyncAreas.length });
-
-  process.on('SIGINT', () => {
-    progressBar.stop();
-  });
-
   let syncCount = 0;
 
-  for (const unsyncArea of unsyncAreas) {
-    if (options?.signal?.aborted) {
-      break;
-    }
+  queue.on('completed', () => {
+    progressBar.increment();
+  });
 
-    progressBar.increment({ syncCount });
+  unsyncAreas.forEach((unsyncArea) => {
+    const task: Task = async ({ signal }) => {
+      if (signal?.aborted) {
+        return;
+      }
 
-    // Find the area data
-    let syncCode;
+      // Find the area data
+      let syncCode;
 
-    switch (area) {
-      case 'provinces': {
-        const matchProvince = await db.query.provinces.findFirst({
-          where: (provinces, { ilike }) =>
-            ilike(provinces.name, `${unsyncArea.PROVINSI}%`),
-        });
+      switch (area) {
+        case 'provinces': {
+          const matchProvince = await db.query.provinces.findFirst({
+            where: (provinces, { ilike }) =>
+              ilike(provinces.name, `${unsyncArea.PROVINSI}%`),
+          });
 
-        if (matchProvince) {
-          syncCode = matchProvince.code;
+          if (matchProvince) {
+            syncCode = matchProvince.code;
+          }
+
+          break;
         }
-
-        break;
-      }
-      case 'regencies': {
-        const matchRegencies = await db
-          .select()
-          .from(regencies)
-          .innerJoin(provinces, eq(regencies.provinceCode, provinces.code))
-          .where(
-            or(
-              ilike(regencies.code, unsyncArea.KODE_KK as string),
-              and(
-                ilike(provinces.name, `%${unsyncArea.PROVINSI}%`),
-                ilike(regencies.name, `%${unsyncArea.KAB_KOTA}%`),
+        case 'regencies': {
+          const matchRegencies = await db
+            .select()
+            .from(regencies)
+            .innerJoin(provinces, eq(regencies.provinceCode, provinces.code))
+            .where(
+              or(
+                ilike(regencies.code, unsyncArea.KODE_KK as string),
+                and(
+                  ilike(provinces.name, `%${unsyncArea.PROVINSI}%`),
+                  ilike(regencies.name, `%${unsyncArea.KAB_KOTA}%`),
+                ),
               ),
-            ),
-          );
+            );
 
-        syncCode = (
-          matchRegencies.length > 1
-            ? matchRegencies.find(
-                ({ regencies }) =>
-                  regencies.code === unsyncArea.KODE_KK ||
-                  regencies.name
-                    .toUpperCase()
-                    .endsWith((unsyncArea.KAB_KOTA as string).toUpperCase()),
-              )
-            : matchRegencies[0]
-        )?.regencies.code;
+          syncCode = (
+            matchRegencies.length > 1
+              ? matchRegencies.find(
+                  ({ regencies }) =>
+                    regencies.code === unsyncArea.KODE_KK ||
+                    regencies.name
+                      .toUpperCase()
+                      .endsWith((unsyncArea.KAB_KOTA as string).toUpperCase()),
+                )
+              : matchRegencies[0]
+          )?.regencies.code;
 
-        break;
-      }
-      case 'districts': {
-        const matchDistricts = await db
-          .select()
-          .from(districts)
-          .innerJoin(regencies, eq(districts.regencyCode, regencies.code))
-          .innerJoin(provinces, eq(regencies.provinceCode, provinces.code))
-          .where(
-            or(
-              ilike(districts.code, unsyncArea.KODE_KEC as string),
-              and(
-                ilike(provinces.name, `%${unsyncArea.PROVINSI}%`),
-                ilike(regencies.name, `%${unsyncArea.KAB_KOTA}%`),
-                ilike(districts.name, `%${unsyncArea.KECAMATAN}%`),
+          break;
+        }
+        case 'districts': {
+          const matchDistricts = await db
+            .select()
+            .from(districts)
+            .innerJoin(regencies, eq(districts.regencyCode, regencies.code))
+            .innerJoin(provinces, eq(regencies.provinceCode, provinces.code))
+            .where(
+              or(
+                ilike(districts.code, unsyncArea.KODE_KEC as string),
+                and(
+                  ilike(provinces.name, `%${unsyncArea.PROVINSI}%`),
+                  ilike(regencies.name, `%${unsyncArea.KAB_KOTA}%`),
+                  ilike(districts.name, `%${unsyncArea.KECAMATAN}%`),
+                ),
               ),
-            ),
-          );
+            );
 
-        syncCode = (
-          matchDistricts.length > 1
-            ? matchDistricts.find(
-                ({ districts }) => districts.code === unsyncArea.KODE_KEC,
-              ) ??
-              matchDistricts.find(
-                ({ districts }) =>
-                  districts.name.toUpperCase() ===
-                  unsyncArea.KECAMATAN?.toUpperCase(),
-              )
-            : matchDistricts[0]
-        )?.districts.code;
+          syncCode = (
+            matchDistricts.length > 1
+              ? matchDistricts.find(
+                  ({ districts }) => districts.code === unsyncArea.KODE_KEC,
+                ) ??
+                matchDistricts.find(
+                  ({ districts }) =>
+                    districts.name.toUpperCase() ===
+                    unsyncArea.KECAMATAN?.toUpperCase(),
+                )
+              : matchDistricts[0]
+          )?.districts.code;
 
-        break;
-      }
-      case 'villages': {
-        const matchVillages = await db
-          .select()
-          .from(villages)
-          .innerJoin(districts, eq(villages.districtCode, districts.code))
-          .innerJoin(regencies, eq(districts.regencyCode, regencies.code))
-          .innerJoin(provinces, eq(regencies.provinceCode, provinces.code))
-          .where(
-            or(
-              ilike(villages.code, unsyncArea.KODE_KD as string),
-              and(
-                ilike(provinces.name, `%${unsyncArea.PROVINSI}%`),
-                ilike(regencies.name, `%${unsyncArea.KAB_KOTA}%`),
-                ilike(districts.name, `%${unsyncArea.KECAMATAN}%`),
-                ilike(villages.name, `%${unsyncArea.NAME}%`),
+          break;
+        }
+        case 'villages': {
+          const matchVillages = await db
+            .select()
+            .from(villages)
+            .innerJoin(districts, eq(villages.districtCode, districts.code))
+            .innerJoin(regencies, eq(districts.regencyCode, regencies.code))
+            .innerJoin(provinces, eq(regencies.provinceCode, provinces.code))
+            .where(
+              or(
+                ilike(villages.code, unsyncArea.KODE_KD as string),
+                and(
+                  ilike(provinces.name, `%${unsyncArea.PROVINSI}%`),
+                  ilike(regencies.name, `%${unsyncArea.KAB_KOTA}%`),
+                  ilike(districts.name, `%${unsyncArea.KECAMATAN}%`),
+                  ilike(villages.name, `%${unsyncArea.NAME}%`),
+                ),
               ),
-            ),
-          );
+            );
 
-        syncCode = (
-          matchVillages.length > 1
-            ? matchVillages.find(
-                ({ villages }) => villages.code === unsyncArea.KODE_KD,
-              ) ??
-              matchVillages.find(
-                ({ villages }) =>
-                  villages.name.toUpperCase() ===
-                  unsyncArea.NAME?.toUpperCase(),
-              )
-            : matchVillages[0]
-        )?.villages.code;
+          syncCode = (
+            matchVillages.length > 1
+              ? matchVillages.find(
+                  ({ villages }) => villages.code === unsyncArea.KODE_KD,
+                ) ??
+                matchVillages.find(
+                  ({ villages }) =>
+                    villages.name.toUpperCase() ===
+                    unsyncArea.NAME?.toUpperCase(),
+                )
+              : matchVillages[0]
+          )?.villages.code;
 
-        break;
+          break;
+        }
       }
-    }
 
-    if (!syncCode) {
-      continue;
-    }
+      if (!syncCode) {
+        return;
+      }
 
-    // Update the area data
-    await db
-      .update(boundaries)
-      .set({
-        sync: true,
-        syncCode: syncCode as string,
-        syncedAt: new Date(),
-      })
-      .where(
-        and(eq(boundaries.FID, unsyncArea.FID), eq(boundaries.area, area)),
-      );
+      // Update the area data
+      await db
+        .update(boundaries)
+        .set({
+          sync: true,
+          syncCode: syncCode as string,
+          syncedAt: new Date(),
+        })
+        .where(
+          and(eq(boundaries.FID, unsyncArea.FID), eq(boundaries.area, area)),
+        );
 
-    syncCount += 1;
-  }
+      syncCount += 1;
+    };
+
+    queue.add(task, { signal: options?.signal }).catch((error) => {
+      if (!(error instanceof DOMException)) {
+        throw error;
+      }
+    });
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    queue.on('idle', () => {
+      resolve();
+    });
+
+    queue.on('error', (error) => {
+      progressBar.stop();
+
+      // Aborted
+      if (error instanceof DOMException) {
+        resolve();
+      }
+
+      // Other errors
+      reject(error);
+    });
+  });
 
   console.log(`Synced ${syncCount} ${area} boundaries`);
 };
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,6 +5,7 @@ const schema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
+  CONCURRENCY: z.coerce.number().int().min(1).default(10),
 });
 
 export type Env = z.infer<typeof schema>;
